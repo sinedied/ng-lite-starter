7eed8ef9216ccd464df5aede12b944f07d6dd3c0
sinedied <noda@free.fr>
2022-07-18 09:42:42 +0200

    chore: move back app folder

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import 'zone.js';
 import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 
 if (environment.production) {
